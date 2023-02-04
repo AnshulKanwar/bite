cdd8eb0a2b28bd9a0edf79daabff32f398b3dd13
Anshul Kanwar <4n5hu1@protonmail.com>
2023-02-05 05:10:53 +0530

    added calorie tracker

---
+++ components/ProgressBar.js
@@ -0,0 +1,25 @@
+import { StyleSheet, View } from "react-native"
+
+const ProgressBar = ({ progress }) => {
+  return (
+    <View style={styles.bar}>
+      <View style={[styles.fill, { width: progress * 100}]}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  bar: {
+    backgroundColor: "#b2b7c6",
+    borderRadius: 999,
+    width: 100,
+    height: 6,
+  },
+  fill: {
+    backgroundColor: "#f3d062",
+    height: 6,
+    borderRadius: 999,
+  }
+})
+
+export default ProgressBar
